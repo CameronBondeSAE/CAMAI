ba21691a77a0ed34fd952b40d6811dfa2572e7b3
Cameron Bonde <c.bonde@sae.edu>
2019-02-06 16:48:26 +1100

    Damager in main scene

--- Assets/Characters/Cam/MrDudes_Model.cs
+++ Assets/Characters/Cam/MrDudes_Model.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class MrDudes_Model : CharacterBase
 {
+	public override void Start()
+	{
+		base.Start();
 
+		GetComponent<Health>().OnHurtEvent += MrDudes_Model_OnHurtEvent;
+	}
+
+	private void MrDudes_Model_OnHurtEvent()
+	{
+		transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+	}
 }
