198b886fc3cf9e72343ff6accaf1126666563fd6
Uncanny <notebucket@yahoo.com>
2019-02-07 14:20:33 +1100

    josh work
    added state machine stuff

---
+++ Assets/Characters/josh/states/StateBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Josh
+{
+    public class StateBase : MonoBehaviour
+    {
+        public virtual void Enter()
+        {
+        
+        }
+
+        // Update is called once per frame
+        public virtual void Execute()
+        {
+        
+        }
+
+        public virtual void Exit()
+        {
+        
+        }
+    }
+}
