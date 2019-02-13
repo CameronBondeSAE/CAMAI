d75987315e57332ac02bc050507bcee1af7be54e
Harry <1011422@student.sae.edu.au>
2019-02-13 17:10:59 +1100

    Messing with braches

--- Assets/Characters/Harry/Kennith_Controller.cs
+++ Assets/Characters/Harry/Kennith_Controller.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using NodeCanvas.BehaviourTrees;
+using NodeCanvas.Tasks.Actions;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 namespace Kennith
 {
@@ -14,12 +17,15 @@ namespace Kennith
 
         public void EvaluateNextMove()
         {
-            if (kennith.currentState == kennith.spiritBombState) kennith.ChangeState(kennith.moveState); 
-            else if (kennith.currentState == kennith.moveState) kennith.ChangeState(kennith.fleeState); 
-            else if (kennith.currentState == kennith.fleeState) kennith.ChangeState(kennith.idleState); 
-            else if (kennith.currentState == kennith.idleState) kennith.ChangeState(kennith.spiritBombState); 
+            
+            GetComponent<BehaviourTreeOwner>().Tick();
+            
+//            if (kennith.currentState == kennith.spiritBombState) kennith.ChangeState(kennith.moveState); 
+//            else if (kennith.currentState == kennith.moveState) kennith.ChangeState(kennith.fleeState); 
+//            else if (kennith.currentState == kennith.fleeState) kennith.ChangeState(kennith.idleState); 
+//            else if (kennith.currentState == kennith.idleState) kennith.ChangeState(kennith.spiritBombState);
         }
         
     }
-
+    
 }
