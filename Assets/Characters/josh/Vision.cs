e2e48267f739df0e0ca50db93d499cc14ea247a0
Uncanny <notebucket@yahoo.com>
2019-02-14 17:07:15 +1100

    JS progress
    going to work on the rest tommorrow

---
+++ Assets/Characters/josh/Vision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    public float ViewDist = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<CharacterBase> UpdateVision()
+    {
+        List<CharacterBase> found = new List<CharacterBase>();
+        RaycastHit ray;
+        foreach (CharacterBase item in GameManager.Instance.CharacterBases)
+        {
+            if (item.gameObject != gameObject)
+            {
+                if (Physics.Raycast(gameObject.transform.position, (item.gameObject.transform.position - gameObject.transform.position).normalized*ViewDist, out ray))
+                {
+                    if (ray.collider.gameObject == item.gameObject)
+                    {
+                        found.Add(item);
+                    }
+                }
+            }
+        }
+
+        return found;
+    }
+}
