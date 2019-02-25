9a5b2c50044649d37dfebd7a2e0d34c96103b5f3
Kyllarr123 <russell.ayres.90@gmail.com>
2019-02-25 23:06:42 +1100

    New Field Of View and Targeting - Editor Buttons for getting all targets

---
+++ Assets/Characters/Russell/FieldOfView.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    public float radius;
+    [UnityEngine.Range(0,360)]
+    public float viewAngle;
+
+    public List<Transform> visibleTargets = new List<Transform>();
+    
+    public Vector3 DirectionAngles(float angleDegree, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleDegree += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleDegree * Mathf.Deg2Rad),0,Mathf.Cos((angleDegree*Mathf.Deg2Rad)));
+    }
+
+    public void FindVisibleTargets()
+    {        
+        visibleTargets.Clear();
+        Collider[] charactersICanSee = Physics.OverlapSphere(transform.position, radius);
+        for (int i = 0; i < charactersICanSee.Length; i++)
+        {
+            
+            Transform target = charactersICanSee[i].transform;
+            CharacterBase cb = target.GetComponent<CharacterBase>();
+            if (cb != null && cb != this)
+            {
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                {
+                    RaycastHit hit;
+                    float disToTarget = Vector3.Distance(transform.position, target.position);
+                    if (Physics.Raycast(transform.position, dirToTarget, out hit, disToTarget))
+                    {
+                        if (hit.transform == target)
+                        {
+                            visibleTargets.Add(target);
+                        }
+                        
+                    }
+
+                }
+            }
+
+
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Characters/Russell/Kyllarr_FieldOfViewEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Russell
+{
+    [CustomEditor(typeof(FieldOfView))]
+    public class Kyllarr_FieldOfViewEditor : Editor
+    {
+        // Start is called before the first frame update
+        void OnSceneGUI()
+        {
+            FieldOfView fow = target as FieldOfView;
+            Handles.color = Color.green;
+            Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.radius);
+            Vector3 viewAngleA = fow.DirectionAngles(-fow.viewAngle / 2, false);
+            Vector3 viewAngleB = fow.DirectionAngles(fow.viewAngle / 2, false);
+
+            Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.radius);
+            Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.radius);
+            
+
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            FieldOfView fow = target as FieldOfView;
+            if (GUILayout.Button("Get Targets"))
+            {
+                fow.FindVisibleTargets();
+            }
+        }
+        
+    
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+
+
+}
