088ebc88acfd0faa23c4640f824a8a77a4082e13
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-02-27 16:33:45 +1100

    Created basic forward movement; Altered scene to allow for movement testing

--- Assets/Characters/Tegan/Scripts/States/MovementState.cs
+++ Assets/Characters/Tegan/Scripts/States/MovementState.cs
@@ -6,14 +6,20 @@ namespace Auriel
 {
 	public class MovementState : StateBase
 	{
+		public Rigidbody rb;
+		public float moveSpeed;
+		
 		public override void Enter()
 		{
 			base.Enter();
+			rb = GetComponent<Rigidbody>();
+			moveSpeed = 25;
 		}
 
 		public override void Execute()
 		{
 			base.Execute();
+			rb.AddRelativeForce(0,0, moveSpeed*Time.deltaTime, ForceMode.VelocityChange);
 		}
 
 		public override void Exit()
