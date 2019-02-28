117649a1a801154dd18f2a9a7c36a137af8c5e12
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-02-28 15:16:02 +1100

    Non-working attempt at creating a base of neighbours

---
+++ Assets/Characters/Tegan/Scripts/NearestNeighbours.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Auriel
+{
+   public class NearestNeighbours : MonoBehaviour
+   {
+       public List<CharacterBase> neighbours;
+           
+       // Start is called before the first frame update
+       void Start()
+       {
+            //List<CharacterBase> neighbours = new List<CharacterBase>();
+       }
+
+       // Update is called once per frame
+       void Update()
+       {
+           
+       }
+
+       private void OnTriggerEnter(Collider other)
+       {
+           neighbours.Add(new CharacterBase());
+           
+           foreach (CharacterBase character in neighbours)
+           {
+               Debug.Log("Someone is nearby");
+           }
+       }
+
+       private void OnTriggerExit(Collider other)
+       {
+           neighbours.Remove(new CharacterBase());
+
+           foreach (CharacterBase character in neighbours)
+           {
+               Debug.Log("Someone has moved away");
+           }
+       }
+   }
+}
