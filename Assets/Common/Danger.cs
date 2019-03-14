e0168a9b2b9583f761a7766fb6617679930b3e05
Cameron Bonde <c.bonde@sae.edu>
2019-03-14 12:12:37 +1100

    Danger level component

---
+++ Assets/Common/Danger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danger : MonoBehaviour
+{
+    public delegate void DangerLevelDelegate(GameObject emitter, CharacterBase owner, float radiusOfDanger,
+        float maxDamageAmount);
+
+    public static event DangerLevelDelegate OnDanger;
+
+    public static void OnDangerInvoke(GameObject emitter, CharacterBase owner, float radiusOfDanger, float maxDamageAmount)
+    {
+        OnDanger?.Invoke(emitter, owner, radiusOfDanger, maxDamageAmount);
+    }
+}
