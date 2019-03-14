f6b6cb7e37eec7a56d2a43387333ca562efe26f7
Cameron Bonde <c.bonde@sae.edu>
2019-03-14 11:35:59 +1100

    Fixed syntax bug

--- Assets/Characters/Cam/Tests/TestSPawn.cs
+++ Assets/Characters/Cam/Tests/TestSPawn.cs
@@ -12,18 +12,18 @@ public class TestSPawn : MonoBehaviour
         yield return new WaitForSeconds(1);
         Instantiate(prefab);
 
-        Vector3 localTargetOffset = transform.InverseTransformPoint(target);
-
-        if (localTargetOffset.x < 0)
-        {
-            
-        }    // he's to the left of me
-
-
-            if (localTargetOffset.z > 0)
-            {
-                
-            }    // He's in front of me
+//        Vector3 localTargetOffset = transform.InverseTransformPoint(target);
+//
+//        if (localTargetOffset.x < 0)
+//        {
+//            
+//        }    // he's to the left of me
+//
+//
+//            if (localTargetOffset.z > 0)
+//            {
+//                
+//            }    // He's in front of me
             
             //Needed to comment this out cause it wasent finished - Russ
             //Quaternion
