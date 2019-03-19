c14ed77e450dd15f3480b42d4e71f33dc13503e2
boredomsMuse <1011084@student.sae.edu.au>
2019-03-19 14:01:50 +1100

    Added in SuckerYou

---
+++ Assets/Characters/Kail/SuckerYou/States/SY_AttackState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kail
+{
+    public class Sy_AttackState : StateBase
+    {
+        //starts once its attached to the target
+
+
+        public override void Exit(int nextState)
+        {
+            base.Exit(nextState);
+            //if next state is idle, jumps off, attacks once more, then goes to idle
+            //if next state is run, jumps off and flees
+        }
+    }
+}
