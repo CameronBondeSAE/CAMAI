429df96af339a8c3081fa95b1de85004b4af4be7
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-03-21 20:23:53 +1100

    Working on creating a custom editor for Auriel (to manually switch states)

---
+++ Assets/Characters/Tegan/Scripts/Custom Editor/AurielEditor.cs
@@ -0,0 +1,43 @@
+﻿using NodeCanvas.Tasks.Actions;
+using UnityEngine;
+using UnityEditor;
+
+namespace Auriel
+{
+   public class AurielEditor : EditorWindow
+   {
+      private Auriel_Model auriel;
+
+      [MenuItem("AI's/Auriel")]
+      public static void ShowWindow()
+      {
+         GetWindow<AurielEditor>("Auriel States");
+      }
+
+      void OnGUI()
+      {
+         GUILayout.Label("Manually change Auriel's states (for testing).", EditorStyles.boldLabel);
+
+         if (Selection.activeGameObject == null)
+         {
+            GUILayout.Label("Select an Auriel AI to use this editor");
+         }
+
+         auriel = Selection.activeGameObject.GetComponent<Auriel_Model>();
+
+         if (auriel == null)
+         {
+            GUILayout.Label("Select an Auriel AI to use this editor");
+         }
+         
+         if (GUILayout.Button("Movement State"))
+         {
+            if (auriel != null)
+            {
+               auriel.ChangeState(auriel.movementState);
+               Debug.Log("Movement state activated");
+            }
+         }
+      }
+   }
+}
