8e8212da9fc13020616a473d0b36293c3052e010
Russell <russell.ayres.90@gmail.com>
2019-03-27 16:37:01 +1100

    pathfinding

---
+++ Assets/Characters/Russell/Pathfinding/Grid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Russell
+{
+    public class Grid : MonoBehaviour
+    {
+        public Node[,] grid;
+        public int x;
+        public int y;
+        public Vector2Int gridSize;
+
+        private void Start()
+        {
+            
+        }
+
+        public void GenerateGrid()
+        {
+            grid = new Node[gridSize.x,gridSize.y];
+        }
+    }
+}
---
+++ Assets/Characters/Russell/Pathfinding/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Russell
+{
+    public class Node
+    {
+        public int gCost;
+        public int hCost;
+        public int fCost;
+        public bool isBlocked;
+        public Node parentNode;
+    
+    }
+}
