c5bebb03624a58d4352f53ef522b837f49729429
boredomsMuse <n>
2019-04-03 16:56:59 +1100

    GOAP start

---
+++ Assets/Characters/Kail/GOAP/ActionTheFirst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ReGoap.Unity;
+using UnityEngine;
+
+public class ActionTheFirst : ReGoapAction<string, object>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        preconditions.Set("atfCondition", true);
+        effects.Set("atfEffect", true);
+    }
+}
