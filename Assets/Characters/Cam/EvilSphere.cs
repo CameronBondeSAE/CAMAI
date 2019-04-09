56d4ccd2182f570ef6c57aa5f3fcba645fadfc21
Cameron Bonde <c.bonde@sae.edu>
2019-04-10 01:25:16 +1000

    Evil sphere tester movement

--- Assets/Characters/Cam/EvilSphere.cs
+++ Assets/Characters/Cam/EvilSphere.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EvilSphere : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-//        transform.Translate();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(new Vector3(-1f + (Mathf.PerlinNoise(Time.time, 0) * 2.1f),
+										0,
+										-1f + (Mathf.PerlinNoise(Time.time * 2.2f, 0) * 2.1f))*speed, Space.Self);
+	}
 }
