c141cbeed6432b2139771ecd1f9103dd6d7a92f8
Cameron Bonde <c.bonde@sae.edu>
2019-05-02 16:40:22 +1000

    no message

--- Assets/Misc/ResetScene.cs
+++ Assets/Misc/ResetScene.cs
@@ -7,12 +7,50 @@ using UnityEngine.SceneManagement;
 public class ResetScene : MonoBehaviour
 {
     public float resetTime = 180;
+    public float originalResetTime = 180;
+    SceneSelector sceneSelector;
 
     // Use this for initialization
-    IEnumerator Start()
+    void Awake()
     {
+        sceneSelector = FindObjectOfType<SceneSelector>();
+        originalResetTime = resetTime;
+
+        SceneManager.activeSceneChanged += delegate(Scene sceneOriginal, Scene sceneNew)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ResetOnNewScene());
+        };
+
+//        ResetOnNewScene();
+    }
+
+    private void Update()
+    {
+        // If user is messing with the mouse, don't auto change
+        if (Input.GetAxisRaw("Mouse X") > 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ResetOnNewScene());
+        }
+    }
+
+    private IEnumerator ResetOnNewScene()
+    {
+        // Scenes can add this component to customise the reset time
+        SceneTimingOverride sceneTimingOverride = FindObjectOfType<SceneTimingOverride>();
+        if (sceneTimingOverride != null)
+        {
+            resetTime = sceneTimingOverride.timeToNextScene;
+        }
+        else
+        {
+            resetTime = originalResetTime;
+        }
+
         while (true)
         {
+            Debug.Log("Waiting for : " + resetTime);
             yield return new WaitForSeconds(resetTime);
             NextScene();
         }
@@ -20,14 +58,6 @@ public class ResetScene : MonoBehaviour
 
     void NextScene()
     {
-        SceneManager.LoadScene(0);
-    }
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            NextScene();
-        }
+        if (sceneSelector != null) sceneSelector.LoadNextScene();
     }
 }
---
+++ Assets/Misc/SceneSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour
+{
+    private int nextSceneIndex;
+
+    void Awake()
+    {
+        Debug.Log("Scene swapper awake:" + SceneManager.GetActiveScene().name);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        // Return the current Active Scene in order to get the current Scene name.
+        Scene scene = SceneManager.GetActiveScene();
+
+        nextSceneIndex = scene.buildIndex + 1;
+        if (nextSceneIndex >= EditorBuildSettings.scenes.Length)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
